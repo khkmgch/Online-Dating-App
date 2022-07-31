028a1d49cd42f069cc4408ebc39ca1a4d1345a99
{Kohei <{kohei.kamiguchi@gmail.com}>
2022-07-31 20:54:13 +0900

    third commit

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
   assetsDir: './',
-  publicPath: './',
-
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Online-Dating-App/'
+    : '/'
+  ,
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
